f6884d789340eaa4f48858c807a4214248b34fd1
npy11 <Neeraj.yadav@teknopoint.local>
2024-09-04 19:37:03 +0530

    carousel css added

--- blocks/carousel/carousel-config.js
+++ blocks/carousel/carousel-config.js
@@ -1,15 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 const heroCarousel = {
   // loop: true,
-  slidesPerView: 4,
+  slidesPerView: 1,
   grabCursor: true,
   autoplay: {
     delay: 50000000,
     disableOnInteraction: false,
   },
   navigation: {
-    // nextEl: swiperButtonNext,
-    // prevEl: swiperButtonPrev,
+    nextEl: '<button type="button" title="Play"></button>',
+    prevEl: '<button type="button" title="Play"></button>',
   },
   pagination: {
     // el: swiperPagination,
@@ -17,16 +17,21 @@ const heroCarousel = {
   },
   breakpoints: {
     300: {
-      slidesPerView: 1,
+      slidesPerView: 1.2,
+      slidesPerGroup: 1,
     },
     780: {
-      slidesPerView: 1,
+      slidesPerView: 2.1,
+    //   slidesPerGroup: 4,
     },
     992: {
-      slidesPerView: 1,
+      slidesPerView: 3.2,
+    //   slidesPerGroup: 4,
     },
-    1199: {
-      slidesPerView: 1,
+    1025: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 20,
     },
   },
 };
