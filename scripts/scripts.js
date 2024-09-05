f184aec9530ec2f67840ab357213f1a15f2279ec
npy11 <Neeraj.yadav@teknopoint.local>
2024-09-05 12:29:54 +0530

    article carousel css added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -112,11 +112,24 @@ async function loadEager(doc) {
   }
 }
 
+function autolinkModals(element) {
+  element.addEventListener('click', async (e) => {
+    const origin = e.target.closest('a');
+
+    if (origin && origin.href && origin.href.includes('/modals/')) {
+      e.preventDefault();
+      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
+      openModal(origin.href);
+    }
+  });
+}
+
 /**
  * Loads everything that doesn't need to be delayed.
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
+  autolinkModals(doc);
   const main = doc.querySelector('main');
   await loadBlocks(main);
 
